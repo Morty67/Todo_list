97c38cecaffd321ab602223a1a1272a4cf3602cd
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-03 11:57:33 +0300

    Added all views for Tag and Task models and create urls for them/templates and add templatetags for future search form

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,53 @@
 from django.urls import path
 
-from todo.views import TaskListView
-
+from todo.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
-
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="tasks-create",
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="tasks-update",
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="tasks-delete",
+    ),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag-list",
+    ),
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag-create",
+    ),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update",
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete",
+    ),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,48 @@
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import (
-    Task,
+    Task, Tag,
 )
 
 
 class TaskListView(generic.ListView):
     model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task_list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task_list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task_list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag_list.html")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag_list.html")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag_list.html")
